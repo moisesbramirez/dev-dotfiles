8fcb9440aeca00f37a1a2b06dad93977405e4953
Moises Ramirez <m.ramirez@pm.me>
2026-04-06 22:58:32 -0700

    Add Brewfile and other dev dependencies

--- setup.sh
+++ setup.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+set -e
+
 XDG_CACHE_HOME="$HOME"/.cache
 XDG_CONFIG_HOME="$HOME"/.config
 XDG_DATA_HOME="$HOME"/.local/share
 XDG_STATE_HOME="$HOME"/.local/state
+PNPM_HOME="$XDG_DATA_HOME/pnpm"
 
 mkdir -p "$XDG_CACHE_HOME"
 mkdir -p "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_DATA_HOME"
 mkdir -p "$XDG_STATE_HOME"
+mkdir -p "$PNPM_HOME"
+
+echo "Installing Homebrew packages..."
+brew bundle --file ./Brewfile
 
 stow -t $XDG_CONFIG_HOME config
 stow --ignore=config -t $HOME .
+
+echo "Installing pnpm packages..."
+pnpm -g install tree-sitter-cli
+pnpm approve-builds -g --all
+pnpm -g install @anthropic-ai/claude-code @agentclientprotocol/claude-agent-acp
+
+echo "Setting gh token..."
+if [ -f /run/secrets/gh_token ] && [ -s /run/secrets/gh_token ]; then
+  gh auth login -h github.com --with-token < /run/secrets/gh_token;
+fi
